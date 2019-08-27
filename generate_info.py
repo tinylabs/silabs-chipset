c98a1842f8b8f7d14c893d57366cc245b321eaa7
Elliot Buller <elliot@tinylabs.io>
2019-08-27 12:07:09 -0600

    Add EFR series micros

--- generate_info.py
+++ generate_info.py
@@ -48,8 +48,9 @@ def cryptomap (d, key, val):
     rmhtml = re.compile ('<.*?>')
     val = re.sub (rmhtml, '', val)
     val = val.replace ('"', '')
-    val = val.replace ('-', '')    
-    d[key] = ','.join (val.split (' ')).lower ()
+    val = val.replace ('-', '')
+    if val is not '':
+        d[key] = ','.join (val.split (' ')).lower () + ','
 
 def commap (d, key, val):
     rmhtml = re.compile ('<.*?>')
@@ -67,9 +68,119 @@ def mapds (d, key, val):
     # Remove japanese PDF extension
     d[key] = val.replace ('-jp', '')
 
+def mapble (d, key, val):
+    if val.lower() == 'yes':
+        if key in d.keys():
+            d[key] += 'ble,'
+        else:
+            d[key] = 'ble,'
+def mapble5 (d, key, val):
+    if val.lower() == 'yes':
+        if key in d.keys():
+            d[key] += 'ble5,'
+        else:
+            d[key] = 'ble5,'
+def map2M (d, key, val):
+    if val.lower() == 'yes':
+        if key in d.keys():
+            d[key] += '2M phy,'
+        else:
+            d[key] = '2M phy,'
+def mapLR (d, key, val):
+    if val.lower() == 'yes':
+        if key in d.keys():
+            d[key] += 'lr,'
+        else:
+            d[key] = 'lr,'
+def mapzigbee (d, key, val):
+    if val.lower() == 'yes':
+        if key in d.keys():
+            d[key] += 'zigbee,'
+        else:
+            d[key] = 'zigbee,'
+def mapthread (d, key, val):
+    if val.lower() == 'yes':
+        if key in d.keys():
+            d[key] += 'thread,'
+        else:
+            d[key] = 'thread,'
+def mapP2G (d, key, val):
+    if val.lower() == 'yes':
+        if key in d.keys():
+            d[key] += 'prop-2G,'
+        else:
+            d[key] = 'prop-2G,'
+def mapPSub (d, key, val):
+    if val.lower() == 'yes':
+        if key in d.keys():
+            d[key] += 'prop-SubG,'
+        else:
+            d[key] = 'prop-SubG,'
+
+def rxCurr (d, key, val):
+    d[key] = val + 'mA'
+    
+def mapOutputPwr (d, key, val):
+    rmhtml = re.compile ('<.*?>')
+    val = re.sub (rmhtml, '', val)
+    val = val.replace ('"', '')
+    val = val.strip ()
+    d[key] = ','.join (val.split (' ')).lower () + 'dBm'
+
+def mapFreq (d, key, val):
+    rmhtml = re.compile ('<.*?>')
+    val = re.sub (rmhtml, '', val)
+    val = val.replace ('"', '')
+    val = val.strip ()
+    if val != 'null':
+        d[key] = ','.join (val.split (' ')).lower () + 'MHz'
+
+def mapAES128 (d, key, val):
+    if val.lower() == 'yes':
+        if key in d.keys ():
+            d[key] += 'aes128,'
+        else:
+            d[key] = 'aes128,'
+
+def mapAES256 (d, key, val):
+    if val.lower() == 'yes':
+        if key in d.keys ():
+            d[key] += 'aes256,'
+        else:
+            d[key] = 'aes256,'
+
+def mapECC (d, key, val):
+    if val.lower() == 'yes':
+        if key in d.keys ():
+            d[key] += 'ecc,'
+        else:
+            d[key] = 'ecc,'
+
+def mapSHA1 (d, key, val):
+    if val.lower() == 'yes':
+        if key in d.keys ():
+            d[key] += 'sha1,'
+        else:
+            d[key] = 'sha1,'
+
+def mapSHA2 (d, key, val):
+    if val.lower() == 'yes':
+        if key in d.keys ():
+            d[key] += 'sha2,'
+        else:
+            d[key] = 'sha2,'
+
 mapping = {
+    # Common
     'Part Number' : (dmap, 'NAME'),
     'Data Sheet'  : (mapds, 'DATASHEET'),
+    'Package Type'  : (dmap, 'PACKAGE'),
+    'USB'         : (yesnomap, 'USB'),
+    'Timers (16-bit)'  : (dmap, 'TIMER'),
+    'ADC 1'       : (analogmap, 'ADC'),
+    'DAC'         : (analogmap, 'DAC'),
+    'Comparators' : (dmap, 'COMPARATOR'),
+    # EFM32
     'Kit'         : (mapds, 'REFDES'),
     'MCU Core'    : (coremap, 'CORE'),
     'Core Frequency' : (dmap, 'FREQ'),
@@ -77,23 +188,43 @@ mapping = {
     'RAM (kB)'    : (dmap, 'RAM'),
     'Dig I/O'     : (dmap, 'IO'),
     'Communications'  : (commap, None),
-    'USB'         : (yesnomap, 'USB'),
     'EMIF'        : (dmap, 'EBI'),
     'CAN'         : (dmap, 'CAN'),
-    'Timers (16-bit)'  : (dmap, 'TIMER'),
     'PCA Channels'  : (ignore, None),
     'Internal Oscillator'  : (oscmap, 'OSC_PREC'),
-    'Comparators' : (dmap, 'COMPARATOR'),
     'LIN'         : (ignore, None),
-    'ADC 1'       : (analogmap, 'ADC'),
     'ADC 2'       : (analogmap, 'ADC'),
-    'DAC'         : (analogmap, 'DAC'),
-    'Package Type'  : (dmap, 'PACKAGE'),
     'Package Size (mm)'  : (dmap, 'SIZE'),
     'Cryptography' : (cryptomap, 'CRYPTO'),
     'Ethernet'  : (yesnomap, 'ETH'),
     'LESENSE'  : (ignore, None),
     'Capacitive Sense'  : (yesnomap, 'CAPSENSE'),
+    # EFR32
+    'MCU'         : (coremap, 'CORE'),
+    'MCU Frequency (MHz)' : (dmap, 'FREQ'),
+    'Flash'       : (dmap, 'FLASH'),
+    'RAM'         : (dmap, 'RAM'),
+    'I²C'         : (dmap, 'I2C'),
+    'I²S'         : (dmap, 'I2S'),
+    'SPI'         : (dmap, 'SPI'),
+    'UART'        : (dmap, 'UART'),
+    'USART'       : (dmap, 'USART'),
+    'Bluetooth Low Energy' : (mapble, 'RADIO'),
+    'Bluetooth 5' : (mapble5, 'RADIO'),
+    'Bluetooth 5 2M PHY' : (map2M, 'RADIO'),
+    'Bluetooth 5 LE Long Range' : (mapLR, 'RADIO'),
+    'zigbee' : (mapzigbee, 'RADIO'),
+    'Thread' : (mapthread, 'RADIO'),
+    'Proprietary Sub-GHz' : (mapP2G, 'RADIO'),
+    'Proprietary 2.4 GHz' : (mapPSub, 'RADIO'),
+    'Output Power (dBm)' : (mapOutputPwr, 'RADIO_TXP'),
+    'Frequency Range' : (mapFreq, 'RADIO_FREQ'),
+    'RX Current (mA)' : (rxCurr, 'RADIO_RXI'),
+    'AES-128' : (mapAES128, 'CRYPTO'),
+    'AES-256' : (mapAES256, 'CRYPTO'),
+    'ECC' : (mapECC, 'CRYPTO'),
+    'SHA-1' : (mapSHA1, 'CRYPTO'),
+    'SHA-2' : (mapSHA2, 'CRYPTO'),
     None : (ignore, None),
     }
 
@@ -106,6 +237,7 @@ def map_row (row, dict_map):
     return ret
 
 family = {
+    # EFM32 microcontrollers
     'gecko'   : {
         'manual' : 'https://www.silabs.com/documents/public/reference-manuals/EFM32G-RM.pdf',
         'pagesize' : '512',
@@ -153,10 +285,24 @@ family = {
     'precision': {
         'manual' : '',
         'pagesize' : '512',
-    },        
+    },
+    # EFR32 microcontrollers
+    'blue' : {
+        'manual' : 'https://www.silabs.com/documents/public/reference-manuals/efr32xg1-rm.pdf',
+        'pagesize' : '2048',
+    },
+    'flex' : {
+        'manual' : 'https://www.silabs.com/documents/public/reference-manuals/efr32xg1-rm.pdf',
+        'pagesize' : '2048',
+    },
+    'mighty' : {
+        'manual' : 'https://www.silabs.com/documents/public/reference-manuals/efr32xg1-rm.pdf',
+        'pagesize' : '2048',
+    },
 }
 
 decoder_ring = [
+    # EFM32 microcontroller products
     { 'match'  : r'efm32gg1',
       'family' : 'giant-s1',
       're'     : r'(efm32gg1.b(...)f\d+)(.).+',
@@ -217,6 +363,22 @@ decoder_ring = [
       're'     : r'(sim3.(.)..)-.-(.).*',
       'val'    : ('gdb_name', 'feature', 'temp', None),
     },
+    # EFR32 microcontroller w/ radio
+    { 'match'  : r'efr32bg1',
+      'family' : 'blue',
+      're'     : r'(efr32bg(\d+)([p|b|v]))(\d{3})(f\d+)([g|i]).+',
+      'val'    : ('gdb_start', 'series', 'perf', 'feature', 'gdb_end', 'temp', None),
+    },
+    { 'match'  : r'efr32fg1',
+      'family' : 'flex',
+      're'     : r'(efr32fg(\d+)([p|b|v]))(\d{3})(f\d+)([g|i]).+',
+      'val'    : ('gdb_start', 'series', 'perf', 'feature', 'gdb_end', 'temp', None),
+    },
+    { 'match'  : r'efr32mg1',
+      'family' : 'mighty',
+      're'     : r'(efr32mg(\d+)([p|b|v]))(\d{3})(f\d+)([g|i]).+',
+      'val'    : ('gdb_start', 'series', 'perf', 'feature', 'gdb_end', 'temp', None),
+    },
 ]
 
 def decode (name, ring):
@@ -267,7 +429,13 @@ if __name__ == '__main__':
             nrow['TYPE'] = 'chipset'
             name = nrow['NAME']
             #print ("=> %s" % name)
-            
+
+            # Trim commas
+            if 'RADIO' in nrow.keys ():
+                nrow['RADIO'] = nrow['RADIO'][:-1]
+            if 'CRYPTO' in nrow.keys ():
+                nrow['CRYPTO'] = nrow['CRYPTO'][:-1]
+
             # Decode name info to family
             info = decode (name, decoder_ring) 
             if 'temp' not in info:
@@ -277,6 +445,10 @@ if __name__ == '__main__':
             elif info['temp'] == 'i':
                 nrow['TEMP'] = '-40,125'
 
+            # Fix gdbname for EFR
+            if 'gdb_name' not in info.keys():
+                info['gdb_name'] = info['gdb_start'] + 'xxx' + info['gdb_end']
+                
             # Save GDB name and interface
             nrow['CMFLAGS'] = 'GDB_NAME:' + info['gdb_name']
             nrow['CMFLAGS'] += ',GDB_IF:swd'
